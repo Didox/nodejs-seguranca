be0abce07f8fc1f8ef806fd5844617d2b974df81
Danilo Santos <danilo.aparecido.santos@gmail.com>
2021-05-12 11:57:03 -0300

    autenticado

--- src/controllers/usuarios_controller.js
+++ src/controllers/usuarios_controller.js
@@ -4,34 +4,26 @@ const autenticado = (req) => { return Cookie.get(req, "usuario") }
 
 const UsuariosController = {
   index: async (req, res) => {
-    if(!autenticado(req)){
-      return res.redirect("/login");
-    }
+    if(!autenticado(req)) return res.redirect("/login");
     
     const sucesso = await req.consumeFlash('sucesso');
     res.render('usuarios/index', { sucesso: sucesso, usuarios: (await Usuario.todos()) });
   },
   excluir: async (req, res) => {
-    if(!autenticado(req)){
-      return res.redirect("/login");
-    }
+    if(!autenticado(req)) return res.redirect("/login");
 
     await Usuario.excluir(req.params.id);
     await req.flash('sucesso', 'Usuário excluido com sucesso');
     res.redirect("/");
   },
   editar: async (req, res) => {
-    if(!autenticado(req)){
-      return res.redirect("/login");
-    }
+    if(!autenticado(req)) return res.redirect("/login");
     
     res.render('usuarios/editar', { usuario: (await Usuario.find(req.params.id)) });
   },
   atualizar: async (req, res) => {
-    if(!autenticado(req)){
-      return res.redirect("/login");
-    }
-
+    if(!autenticado(req)) return res.redirect("/login");
+    
     let usuario = new Usuario(req.body);
     usuario.id = req.params.id;
     await usuario.salvar();
