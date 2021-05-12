39b2b70b66aceae819b2cbb1967436438a0c0121
Danilo Santos <danilo.aparecido.santos@gmail.com>
2021-05-12 11:37:11 -0300

    correcao sql injection

--- src/models/usuario.js
+++ src/models/usuario.js
@@ -10,8 +10,8 @@ module.exports = class Cliente{
   }
 
   static async login(email, senha){
-    let query = `SELECT * FROM usuarios where email = '${email}' and senha = '${senha}' `;
-    let usuarios = await db.exec(query);
+    let query = "SELECT * FROM usuarios where email = ? and senha = ?";
+    let usuarios = await db.exec(query, [email, senha]);
     if(usuarios.length == 0) return undefined
     return usuarios[0];
   }
@@ -22,22 +22,25 @@ module.exports = class Cliente{
   }
 
   static async excluir(id){
-    await db.exec(`delete from usuarios where id = ${id}`);
+    await db.exec("delete from usuarios where id = ?", [id]);
   }
 
   async salvar(){
     let sql = "";
+    let values = [];
     if(this.id && this.id > 0){
-      sql = `update usuarios set nome='${this.nome}', email='${this.email}', descricao='${this.descricao}', senha='${this.senha}' where id=${this.id}`;
+      sql = "update usuarios set nome=?, email=?, descricao=?, senha=? where id=?";
+      values = [this.nome, this.email, this.descricao, this.senha, this.id];
     }
     else{
-      sql = `insert into usuarios(nome, email, descricao, senha) values('${this.nome}', '${this.email}', '${this.descricao}', '${this.senha}')`;
+      sql = "insert into usuarios(nome, email, descricao, senha) values(?,?,?,?)";
+      values = [this.nome, this.email, this.descricao, this.senha];
     }
-    await db.exec(sql);
+    await db.exec(sql, values);
   }
 
   static async find(id){
-    let usuarios = await db.exec(`SELECT * FROM usuarios where id= ${id}`);
+    let usuarios = await db.exec("SELECT * FROM usuarios where id=?", [id]);
     return usuarios[0];
   }
 };
